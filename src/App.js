b88672981932839fdf361cc9f0ff8c7b45107601
Ashish Chawda <ashish11chawda@gmail.com>
2021-06-02 07:49:16 +0530

    complete app

--- src/App.js
+++ src/App.js
@@ -1,12 +1,35 @@
-import './App.css';
-import Form from './components/form'
+import React from "react";
+import "./App.css";
+import Form from "./components/form";
+import firebase from "firebase/app";
+import "firebase/auth";
+import "firebase/analytics";
+import Header from "./components/header";
+import { useAuthState } from "react-firebase-hooks/auth";
+import About from "./components/about";
+import GCRLogo from "./images/crf21-main-logo.png";
 
-function App() {
+const auth = firebase.auth();
+const analytics = firebase.analytics();
+
+
+export default function App() {
+  const [userAuth] = useAuthState(auth);
+
   return (
     <div className="App">
-      <Form/>
+      <Header user={userAuth} />
+      <div
+        style={{
+          display: "flex",
+          width: "100%",
+          flexWrap: "wrap",
+          justifyContent: "center",
+          margin: "30px 0",
+        }}>
+        <img alt="Google Cloud Ready Facilitator Program Logo" src={GCRLogo} width="30%" height="auto" />
+      </div>
+      <section>{userAuth ? <Form /> : <About />}</section>
     </div>
   );
 }
-
-export default App;
